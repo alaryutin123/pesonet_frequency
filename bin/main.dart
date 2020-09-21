7eed09c8946571f1d34b9b4629d389891963d228
Alex <alex.laryutin@gmail.com>
2020-09-21 16:05:56 +0800

    Adding recepeint frequency

--- bin/main.dart
+++ bin/main.dart
@@ -1,4 +1,3 @@
-
 import 'dart:io';
 
 import 'package:args/args.dart';
@@ -7,31 +6,41 @@ import 'package:pesonet_frequency/pesonet_frequency.dart';
 void main(List<String> arguments) async {
   final argParser = ArgParser();
 
-  print ('This is the PESONET CSV files parser. It identifies if there are acconts (combination of BIC and Account Number) \n '
-  + 'in the provided files that violate the more or equal than <frequency> parameter.'
-  + '\n\n Usage options:\n');
+  print('This is the PESONET CSV files parser. It identifies if there are acconts (combination of BIC and Account Number) \n ' +
+      'in the provided files that violate the more or equal than <frequency> parameter.' +
+      '\n\n Usage options:\n');
 
-  argParser.addOption("file", valueHelp: "filename",defaultsTo: ".",help: "File to process. Can only be a .CSV file from Pesonet or the directory containing the CSV files.");
-  argParser.addOption("freq",valueHelp: "frequency", defaultsTo: "3", help: "Check the frequency. Any account with above or equal to this frequency of entries will be highlighted in the script output");
+  argParser.addOption("file",
+      valueHelp: "filename",
+      defaultsTo: ".",
+      help:
+          "File to process. Can only be a .CSV file from Pesonet or the directory containing the CSV files.");
+  argParser.addOption("freq",
+      valueHelp: "frequency",
+      defaultsTo: "3",
+      help:
+          "Check the frequency. Any account with above or equal to this frequency of entries will be highlighted in the script output");
 
-  print (argParser.usage);
+  print(argParser.usage);
 
   final results = argParser.parse(arguments);
   final String fileName = results["file"];
   final int frequency = int.parse(results["freq"]);
   print('---------------------------------------------------------------\n\n');
-  print('Running the script with the following parameters: File/Dir: \'$fileName\', Checked frequency: $frequency');
-  print('\n---------------------------------------------------------------\n\n');
+  print(
+      'Running the script with the following parameters: File/Dir: \'$fileName\', Checked frequency: $frequency');
+  print(
+      '\n---------------------------------------------------------------\n\n');
 
   await Worker(path: fileName, frequency: frequency).run();
 
-  print('Press any key to exit');
+  _awaitExit();
+}
 
-  var input = stdin;
+void _awaitExit() {
+  print('\n\nPress any key to exit');
 
-  var subs;
-  subs = input.listen((line) {
+  stdin.listen((line) {
     exit(0);
   });
-
 }
--- lib/pesonet_frequency.dart
+++ lib/pesonet_frequency.dart
@@ -1,24 +1,30 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:csv/csv.dart';
+import 'package:intl/intl.dart';
+import 'package:logging/logging.dart';
 
 class Worker {
   final String path;
   final int frequency;
-  final records = Map<String, int>();
+  final senderFrequencyRecords = Map<String, int>();
+  final receiverFrequencyRecords = Map<String, int>();
+
+  final outputFile =
+      File('${DateFormat('yyyyMMdd-H:m:ss').format(DateTime.now())}.log');
+  final Logger logger = new Logger('PesonetFrequency');
 
   Worker({this.path = ".", this.frequency = 3});
 
   void run() async {
-    records.clear();
-    print('Opening the dir');
+    _initLogger();
     if (path.endsWith(".csv")) {
       //Assuming we are working with a single file
       try {
         await processFile(path);
       } on FileSystemException catch (e) {
         print(
-            'Error occured when processing file \'${e.path}\'. Please make sure it\'s a valid '+
+            'Error occured when processing file \'${e.path}\'. Please make sure it\'s a valid ' +
                 'PESONET file in CSV format.\n Error: ${e.message}');
         exit(-1);
       }
@@ -26,6 +32,7 @@ class Worker {
       //Assuming we are working with a directory
       try {
         //Trying to open a directory and list the files
+        _log('Processing CSV files in dir $path');
         final dir = Directory(path).listSync();
         for (int i = 0; i < dir.length; i++) {
           if (dir[i].path.toLowerCase().endsWith(".csv")) {
@@ -37,48 +44,85 @@ class Worker {
         print('Error occured reading directory ${path}. Error : ${e.message}');
       }
     }
-
     //Counting lines
-    processingOutput();
+    _senderFrequencyCheck();
+    _receiverFrequencyCheck();
   }
 
-  void processingOutput() {
+  void _senderFrequencyCheck() {
+    _log('\n\nSending frequency check (BIC:Account)');
+    _log('--------------------------------------------------');
+    int count =
+        _printSortedMapFrequency('Sending account ', senderFrequencyRecords);
+
+    _log(
+        '\nTotal records processed: ${senderFrequencyRecords.length}; Total velocity violations of ' +
+            '${frequency} entries per sending account: ${count}\n\n');
+  }
+
+  void _receiverFrequencyCheck() {
+    _log('\n\nReceiving frequency check (MIN)');
+    _log('--------------------------------------------------');
+    int count =
+        _printSortedMapFrequency('Receiver MIN ', receiverFrequencyRecords);
+
+    _log(
+        '\nTotal records processed: ${senderFrequencyRecords.length}; Total velocity violations of ' +
+            '${frequency} entries per sending account: ${count}\n\n');
+  }
+
+  int _printSortedMapFrequency(String accountName, Map<String, int> map) {
     int count = 0;
 
     final sortedMap = Map.fromEntries(
-        records.entries.toList()
-          ..sort((e1, e2) => e2.value.compareTo(e1.value)));
+        map.entries.toList()..sort((e1, e2) => e2.value.compareTo(e1.value)));
 
     sortedMap.forEach((key, value) {
       if (value >= frequency) {
-        print('Account $key has ${value} entries');
+        _log('$accountName $key has ${value} entries');
         count++;
       }
     });
+    return count;
+  }
 
-    print('Total records processed: ${records
-        .length}; Total velocity violations of '
-        + '${frequency} entries per account: ${count}');
+  void _initLogger() {
+    Logger.root.level = Level.ALL;
+    Logger.root.onRecord.listen((LogRecord rec) {
+      outputFile.writeAsStringSync("${rec.message}\n", mode: FileMode.append);
+    });
   }
 
   Future processFile(String path) async {
     final input = new File(path).openRead();
-    print('Processing file: ${path}');
+    _log('Processing file $path');
     final fields = await input
         .transform(utf8.decoder)
         .transform(new CsvToListConverter())
         .toList();
-    processListOfCSVRecords(fields);
+    processCSVRecordsFrequency(fields);
   }
 
-  void processListOfCSVRecords(List<List> fields) {
-    for (var i = 1; i < fields.length; i++) {
+  void processCSVRecordsFrequency(List<List> fields) {
+    for (var i = 0; i < fields.length; i++) {
       final account = '${fields[i][3]}:${fields[i][14]}';
-      if (records.containsKey(account)) {
-        records[account]++;
-      } else {
-        records[account] = 1;
-      }
+      _processFrequency(account, senderFrequencyRecords);
+      var min = '${fields[i][15]}';
+      min = min.startsWith('0') ? min.substring(1) : min;
+      _processFrequency(min, receiverFrequencyRecords);
     }
+  }
+
+  void _processFrequency(String account, Map<String, int> records) {
+    if (records.containsKey(account)) {
+      records[account]++;
+    } else {
+      records[account] = 1;
+    }
+  }
+
+  void _log(String s) {
+    print(s);
+    logger.info(s);
   }
 }
