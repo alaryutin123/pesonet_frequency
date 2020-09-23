bdbe7ead9b98b0d346eca260fffba3c6ab617a91
Alex <alex.laryutin@gmail.com>
2020-09-23 14:49:47 +0800

    Modifying according to the requirements

--- lib/pesonet_frequency.dart
+++ lib/pesonet_frequency.dart
@@ -5,13 +5,16 @@ import 'package:intl/intl.dart';
 import 'package:logging/logging.dart';
 
 class Worker {
+  final List<String> whitelisted = ['DBPHPHMMXXX:010590018'];
   final String path;
   final int frequency;
-  final senderFrequencyRecords = Map<String, int>();
-  final receiverFrequencyRecords = Map<String, int>();
+  final senderFrequencyRecords = Map<String, Records>();
+  final receiverFrequencyRecords = Map<String, Records>();
+  final blacklisted = List<String>();
 
   final outputFile =
-      File('${DateFormat('yyyyMMdd-H:m:ss').format(DateTime.now())}.log');
+      File('${DateFormat('yyyyMMdd-HH:mm:ss').format(DateTime.now())}.log');
+
   final Logger logger = new Logger('PesonetFrequency');
 
   Worker({this.path = ".", this.frequency = 3});
@@ -44,43 +47,34 @@ class Worker {
         print('Error occured reading directory ${path}. Error : ${e.message}');
       }
     }
+
+    //Printing blacklisted account
+    _log('\nRecords with violations: \n');
+    blacklisted.forEach((element) {
+      _log('$element,SNDR_FRMT');
+    });
+
     //Counting lines
-    _senderFrequencyCheck();
-    _receiverFrequencyCheck();
+    _printSortedMapFrequency(
+        '\nSender Velocity Limit\n', 'LMT_VLCT_SNDR', senderFrequencyRecords);
+    _printSortedMapFrequency('\nReceiver Velocity Limit\n', 'LMT_VLCT_RCVR',
+        receiverFrequencyRecords);
   }
 
-  void _senderFrequencyCheck() {
-    _log('\n\nSending frequency check (BIC:Account)');
-    _log('--------------------------------------------------');
-    int count =
-        _printSortedMapFrequency('Sending account ', senderFrequencyRecords);
-
-    _log(
-        '\nTotal records processed: ${senderFrequencyRecords.length}; Total velocity violations of ' +
-            '${frequency} entries per sending account: ${count}\n\n');
-  }
-
-  void _receiverFrequencyCheck() {
-    _log('\n\nReceiving frequency check (MIN)');
-    _log('--------------------------------------------------');
-    int count =
-        _printSortedMapFrequency('Receiver MIN ', receiverFrequencyRecords);
-
-    _log(
-        '\nTotal records processed: ${senderFrequencyRecords.length}; Total velocity violations of ' +
-            '${frequency} entries per sending account: ${count}\n\n');
-  }
-
-  int _printSortedMapFrequency(String accountName, Map<String, int> map) {
+  int _printSortedMapFrequency(
+      String title, String code, Map<String, Records> map) {
     int count = 0;
 
-    final sortedMap = Map.fromEntries(
-        map.entries.toList()..sort((e1, e2) => e2.value.compareTo(e1.value)));
-
+    final sortedMap = Map.fromEntries(map.entries.toList()
+      ..sort((e1, e2) => e2.value.count.compareTo(e1.value.count)));
+    // _log(title);
     sortedMap.forEach((key, value) {
-      if (value >= frequency) {
-        _log('$accountName $key has ${value} entries');
-        count++;
+      //If count is more than frequency - print the seqs that are violating
+      if (value.count > frequency) {
+        for (int i = frequency; i < value.seqs.length; i++) {
+          _log('${value.seqs[i]},$code');
+          count++;
+        }
       }
     });
     return count;
@@ -104,20 +98,31 @@ class Worker {
   }
 
   void processCSVRecordsFrequency(List<List> fields) {
-    for (var i = 0; i < fields.length; i++) {
-      final account = '${fields[i][3]}:${fields[i][14]}';
-      _processFrequency(account, senderFrequencyRecords);
-      var min = '${fields[i][15]}';
-      min = min.startsWith('0') ? min.substring(1) : min;
-      _processFrequency(min, receiverFrequencyRecords);
-    }
+    fields.forEach((line) {
+      final seq = line[0];
+      final sender = '${line[14]}';
+      if (sender.trim() == null || sender.trim() == '0') {
+        blacklisted.add(seq);
+      } else {
+        final account = '${line[3]}:${line[14]}';
+
+        if (whitelisted.indexOf(account) < 0) {
+          _processFrequency(seq, account, senderFrequencyRecords);
+        }
+        var min = '${line[15]}';
+        //Removing the heading 0
+        min = min.startsWith('0') ? min.substring(1) : min;
+        _processFrequency(line[0], min, receiverFrequencyRecords);
+      }
+    });
   }
 
-  void _processFrequency(String account, Map<String, int> records) {
+  void _processFrequency(
+      String seq, String account, Map<String, Records> records) {
     if (records.containsKey(account)) {
-      records[account]++;
+      records[account].append(seq);
     } else {
-      records[account] = 1;
+      records[account] = Records(seq);
     }
   }
 
@@ -125,4 +130,21 @@ class Worker {
     print(s);
     logger.info(s);
   }
+}
+
+class Records {
+  var _count = 1;
+  final List<String> _seqs = List<String>();
+
+  Records(String seq) {
+    _seqs.add(seq);
+  }
+
+  void append(String seq) {
+    _count++;
+    _seqs.add(seq);
+  }
+
+  int get count => _count;
+  List<String> get seqs => _seqs;
 }
